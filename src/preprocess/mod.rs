d47ad5d1f33e364a0e39be2bafde69a683010aa1
Taehwan Kim <tthhkimgo@gmail.com>
2026-06-23 12:24:34 +0900

    take 1

--- src/preprocess/mod.rs
+++ src/preprocess/mod.rs
@@ -1,7 +1,5 @@
 pub(crate) mod context;
 pub(crate) mod expression;
 pub(crate) mod parser;
-mod parser_lr_expression_expanded;
-mod parser_lr_file_expanded;
-mod parser_lr_macro_expanded;
+mod parser_lr_expanded;
 pub(crate) mod preprocessor;
